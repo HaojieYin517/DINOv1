f0df89d7796328ed6ede5b058b920480a7becc4d
Yin <hy2108@nyu.edu>
2025-12-01 21:39:32 +0000

    Remove incorrect nested dino/ directory

--- vision_transformer.py
+++ vision_transformer.py
@@ -259,6 +259,19 @@ def vit_small_patch8_512(patch_size=8, **kwargs):
     )
     return model
 
+def vit_base_patch8(patch_size=8, **kwargs):
+    model = VisionTransformer(
+        patch_size=patch_size,
+        embed_dim=704,                # MATCH MAE
+        depth=12,
+        num_heads=11,                  # MATCH MAE
+        mlp_ratio=4,
+        qkv_bias=True,
+        norm_layer=partial(nn.LayerNorm, eps=1e-6),
+        **kwargs
+    )
+    return model
+
 
 def vit_base(patch_size=16, **kwargs):
     model = VisionTransformer(
